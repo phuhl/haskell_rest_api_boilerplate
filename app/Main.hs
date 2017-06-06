5001d8105099c89f799156854c3dcfc44bac9507
Philipp Uhl <git@ph-uhl.com>
2017-06-06 14:54:40 +0200

    Working

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
 module Main where
 
-import Lib
+import qualified RestServer as RS
+
+import           Control.Monad.Except        (ExceptT)
+import           Servant (ServerT, Proxy(..))
+import           Servant.API
+
+import           Data.Text
+
+data Config = Config {}
+
+type API =
+  "v1" :> "test1" :> Get '[JSON] Text
+  :<|> "v1" :> "test2" :> Get '[JSON] Text
+
+
+api :: ServerT API (RS.App Config)
+api = test1 :<|> test2
+
+apiP :: Proxy API
+apiP = Proxy
+
+test1 :: (RS.App Config) Text
+test1 = return "test1"
+
+test2 :: (RS.App Config) Text
+test2 = return "test2"
 
 main :: IO ()
-main = someFunc
+main = RS.createRestServer 8080 (Config) apiP api
--- src/RestServer.hs
+++ src/RestServer.hs
@@ -1,31 +1,26 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-
-
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-
-
-{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE DataKinds #-}
 
 module RestServer
     (
-      createRestServer
+      createRestServer,
+      App(..)
     ) where
 
 
-import           Control.Monad.Reader        (MonadIO, MonadReader,
-                                              ReaderT, runReaderT, asks, liftIO)
-import           Control.Monad.Except        (ExceptT, MonadError)
+import Control.Monad.Reader (MonadIO, MonadReader, ReaderT
+                            , runReaderT, asks, liftIO)
+import Control.Monad.Except (ExceptT, MonadError)
+import Servant              (Server, ServerT, HasServer
+                            , ServantErr, Proxy, serve)
+import Servant.Utils.Enter  (Enter, (:~>)(..), enter)
+import Network.Wai.Handler.Warp (run,Port)
+import Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
 
-import           Servant
-import           Servant.API
-import           Servant.Utils.Enter
-import           Network.Wai.Handler.Warp (run,Port)
-import           Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
 
 newtype App config a
     = App
@@ -34,31 +29,22 @@ newtype App config a
                  MonadError ServantErr, MonadIO)
 
 
-type UsersAPI =
-  "v1" :> "tabs" :> Get '[JSON] String
-
-
-
 convertApp :: config -> (App config) :~> ExceptT ServantErr IO
 convertApp cfg = Nat (flip runReaderT cfg . runApp)
 
-appToServer :: (Enter (ServerT api (App config))
-                ((App config) :~> ExceptT ServantErr IO) (Server api))
-            => config -> ServerT api (App config) -> Server api
-appToServer config serverApi = enter (convertApp config) (serverApi)
-
-api :: Proxy api
-api = Proxy
-
-app :: (Enter (ServerT api (App config))
-                ((App config) :~> ExceptT ServantErr IO) (Server api))
-       => config -> ServerT api (App config) -> Application
-app config serverApi = serve (api :: Proxy api) $ appToServer config serverApi
-
-createRestServer :: Port -> ServerT api (App config) -> config -> IO ()
-createRestServer port serverApi config = do
-  run port $ logStdout $ app config serverApi
-
+createRestServer :: ((Enter
+                      (ServerT api (App config))
+                      ((App config) :~> ExceptT ServantErr IO)
+                      (Server api)
+                     )
+                    , (HasServer api '[]))
+  => Port -> config -> Proxy api -> ServerT api (App config)
+  -> IO ()
+createRestServer port config proxy serverApi =
+  run port
+  $ logStdout
+  $ serve proxy
+  $ enter (convertApp config) (serverApi)
 
 
 {-startServer :: Environment -> IO ()
